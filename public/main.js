bf73640cd270e26b14aa2670c68655df69058fc9
Jared Sewak <jaredsewak@gmail.com>
2023-04-06 16:36:52 -0600

    rollbar-lab

--- public/main.js
+++ public/main.js
@@ -1,5 +1,9 @@
 const btn = document.querySelector('button')
+const btn2 = document.querySelector('#endPoint')
 
 const clickHandler = () => alert('The cat name is: Rascal')
+const clickHandler2 = () => axios.get('/api/endpoint').then(res => alert(res.data))
+
 
 btn.addEventListener('click', clickHandler)
+btn2.addEventListener('click', clickHandler2)
--- server.js
+++ server.js
@@ -1,6 +1,49 @@
 const express = require('express')
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'be3868ebb6ce4abf8f94b8023047fe09',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+app.get('/api/bananas', (req, res) => {
+   try {
+      banana()
+   } catch (theseHands){
+      rollbar.error(theseHands)
+   }
+})
+
+app.get('/api/oranges', (req, res) => {
+   try {
+      oranges()
+   } catch (theseHands){
+      rollbar.critical(theseHands)
+   }
+})
+
+app.get('/api/apples', (req, res) => {
+   try {
+      apples()
+   } catch (theseHands){
+      rollbar.warning(theseHands)
+   }
+})
+
+app.get('/api/endpoint', (req, res) => {
+   try {
+      clickHandler2()
+   } catch (theseHands) {
+      rollbar.critical(theseHands)
+   }
+})
+
 app.use(express.static(`${__dirname}/public`))
 
 app.listen(4000,
